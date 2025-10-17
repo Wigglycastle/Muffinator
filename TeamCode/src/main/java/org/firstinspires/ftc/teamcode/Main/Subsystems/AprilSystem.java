07a326736b323e6d54425ab56703af08b0a7f7b4
Weston Weissert <134028416+Wigglycastle@users.noreply.github.com>
2025-10-17 12:29:59 -0700

    Update PedroPathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/Subsystems/AprilSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/Subsystems/AprilSystem.java
@@ -4,16 +4,15 @@ import static android.os.SystemClock.sleep;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
 public class AprilSystem {
     //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
     //  applied to the drive motors to correct the error.
@@ -134,6 +133,7 @@ public class AprilSystem {
                 backRightPower /= max;
              }
         }
+
     private void setManualExposure(int exposureMS, int gain) {
         // Wait for the camera to be open, then use the controls
 
